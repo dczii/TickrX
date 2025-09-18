abe5bfebdd9fcb81c1780ee01b78c81a2392ce6e
Danilo Zabala II <dczii@live.com>
2025-09-18 17:41:51 +0800

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </div>
         <Toaster position='top-right' richColors />
+        <Analytics />
       </body>
     </html>
   );
